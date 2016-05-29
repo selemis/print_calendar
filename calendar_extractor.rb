78fee8f0afaa73ea3884cab95a3437fa0778470a
Stelios Omirou <selemis@gmail.com>
2016-05-29 21:35:08 +0300

    Refactoring. Separating calander data extraction from pdf creation.

---
+++ calendar_extractor.rb
@@ -0,0 +1,56 @@
+require_relative 'authorizable'
+require_relative 'calendar_print'
+require_relative 'functions'
+require 'awesome_print'
+require 'active_support/core_ext/hash/keys'
+
+##
+# Extract information from google calendar
+
+class CalendarExtractor
+  include Authorizable
+  include Functions
+
+  APPLICATION_NAME = 'Google Calendar API Ruby Quickstart'
+
+  def initialize
+    # Initialize the API
+    @service = Google::Apis::CalendarV3::CalendarService.new
+    @service.client_options.application_name = APPLICATION_NAME
+    @service.authorization = authorize
+  end
+
+  ##
+  # It returns a hash with key the name of the calendar
+  # and value corresponding the CalendarPrint Object.
+  def extract
+    named_calendar_prints = Hash.new
+    named_responses = to_named_responses(@service)
+    named_responses.each do |name, response|
+      named_calendar_prints[name] = to_calendar_prints(response)
+    end
+    named_calendar_prints
+  end
+
+  private
+
+  def to_named_responses(service)
+    named_responses = Hash.new
+    read_calendars.each do |name, calendar|
+      id = calendar.delete(:id)
+      named_responses[name] = service.list_events(id, calendar.symbolize_keys!)
+    end
+    named_responses
+  end
+
+  def to_calendar_prints(response)
+    response.items.map do |item|
+      CalendarPrint.from_hash(title: item.summary,
+                               from: parse_date(item.start),
+                                 to: parse_date(item.end),
+                        description: item.description || '')
+    end
+
+  end
+
+end
--- calendar_pdf_exporter.rb
+++ calendar_pdf_exporter.rb
@@ -1,39 +1,18 @@
 require_relative 'calendar_booklet'
 require_relative 'calendar_summary'
 require_relative 'calendar_print'
-require_relative 'authorizable'
-require_relative 'functions'
-require 'awesome_print'
-require 'active_support/core_ext/hash/keys'
+require_relative 'calendar_extractor'
 
-include Authorizable
-include Functions
-
-APPLICATION_NAME = 'Google Calendar API Ruby Quickstart'
-
-# Initialize the API
-service = Google::Apis::CalendarV3::CalendarService.new
-service.client_options.application_name = APPLICATION_NAME
-service.authorization = authorize
-
-read_calendars.each do |name, calendar|
-  id = calendar.delete(:id)
-  response = service.list_events(id, calendar.symbolize_keys!)
-
-  calendar_prints = response.items.map do |item|
-    CalendarPrint.from_hash(
-              title: item.summary,
-               from: parse_date(item.start),
-                 to: parse_date(item.end),
-        description: item.description || '')
-  end
-
-
+def export_pdf(name, calendar_prints)
   begin
     CalendarSummary.new(calendar_prints).render_file("pdfs/#{name}_summary.pdf")
     CalendarBooklet.new(calendar_prints).render_file("pdfs/#{name}_booklet.pdf")
   rescue Exception => e
     puts e
   end
+end
 
+extractor = CalendarExtractor.new
+extractor.extract.each do |name, calendar_print|
+  export_pdf(name, calendar_print)
 end
